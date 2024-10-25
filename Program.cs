d465f8999394d3a35c6455d06b2e8d618daa4274
sedef <sedef@SEDEF>
2024-10-25 21:25:56 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yazilim251004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir sayı girin");
+            int sayi= Convert.ToInt32(Console.ReadLine());
+            if (sayi >= 0 && sayi <=100)
+            {
+                Console.WriteLine("sayı 0 ile 100 arasındadır");
+            }
+            else
+            {
+                Console.WriteLine("sayı 0 ile 100 arasında değildir");
+            }
+            Console.ReadKey();
+        }
+    }
+}
